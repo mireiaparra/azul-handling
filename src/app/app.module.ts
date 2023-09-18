f3d710653ffa311d6af17f651bff4e4cfe124f72
mireia <mirmalparra@gmail.com>
2023-09-18 19:14:08 +0200

    Add basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { DeparturesComponent } from './components/home/departures/departures.component';
+import { FlightsComponent } from './components/home/flights/flights.component';
+import { ArrivalsComponent } from './components/home/arrivals/arrivals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    HomeComponent,
+    ArrivalsComponent,
+    DeparturesComponent,
+    FlightsComponent
   ],
   imports: [
     BrowserModule,
