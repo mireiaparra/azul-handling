b093ddd9befce2410f4d1ca30a8d79cfc81f10e5
Mireia <mireia@beateam.es>
2023-09-19 13:35:26 +0200

    Add server config

---
+++ back/app.js
@@ -0,0 +1,39 @@
+// Import necessary modules
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'your_user',
+  password: 'your_password',
+  database: 'your_database_name'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database: ' + err);
+  } else {
+    console.log('Connected to the database');
+  }
+});
+
+app.get('/getFlights', (req, res) => {
+  const sql = 'SELECT * FROM flights';
+
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.error('Error executing the query: ' + err);
+      res.status(500).json({ error: 'Error fetching flights' });
+    } else {
+      res.json(result); 
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Express server is listening on port ${port}`);
+});
+
